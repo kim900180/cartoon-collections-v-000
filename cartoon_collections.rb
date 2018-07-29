fda889e4c4dc1e4dd55ff952a5b99bc060b09c57
Jae Yong Kim <kim900180@gmail.com>
2018-07-29 18:55:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,15 @@ def summon_captain_planet(array)# code an argument here
   return new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  i = 0
+  while i < array.length
+    if array[i].length < 4
+      return false
+    else
+      return true
+  end
 end
 
 def find_the_cheese# code an argument here
