21173cc26dd0e680c5f32ad4f2215e518bb5873a
Jae Yong Kim <kim900180@gmail.com>
2018-07-29 18:57:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,10 +27,11 @@ def long_planeteer_calls(array)# code an argument here
   i = 0
   while i < array.length
     if array[i].length < 4
+      false
       i += 1
-      return false
     elsif array[i].length > 4
-      return true
+      true
+      i += 1
     end
   end
 end
